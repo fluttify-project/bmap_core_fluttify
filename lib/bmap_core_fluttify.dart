011f325336f9c2c7e33ac67626693b9d8d2a5e95
yohom <yohombao@qq.com>
2020-02-28 10:26:00 +0800

    fix: 导出类

--- lib/bmap_core_fluttify.dart
+++ lib/bmap_core_fluttify.dart
@@ -1,13 +1,2 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class BmapCoreFluttify {
-  static const MethodChannel _channel =
-      const MethodChannel('bmap_core_fluttify');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'src/android/android.export.g.dart';
+export 'src/ios/ios.export.g.dart';
