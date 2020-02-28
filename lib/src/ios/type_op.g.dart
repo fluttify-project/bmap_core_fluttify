88c05f9fd83318b7bd5214b0f831aa6bde58aa93
yohom <yohombao@qq.com>
2020-02-28 10:14:09 +0800

    roll engine #d0a1b9b

--- lib/src/ios/type_op.g.dart
+++ lib/src/ios/type_op.g.dart
@@ -19,6 +19,41 @@ extension TypeOpBmapCoreFluttifyIOS on Ref {
     return result;
   }
   
+  Future<bool> isBMKCoordinateSpan() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKCoordinateSpan', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKCoordinateBounds() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKCoordinateBounds', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKCoordinateRegion() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKCoordinateRegion', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKGeoPoint() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKGeoPoint', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKMapPoint() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKMapPoint', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKMapSize() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKMapSize', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKMapRect() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKMapRect', {'refId': refId});
+    return result;
+  }
+  
   Future<bool> isBMKPlanNode() async {
     final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKPlanNode', {'refId': refId});
     return result;
@@ -43,6 +78,41 @@ extension TypeOpBmapCoreFluttifyIOS on Ref {
   Future<BMKUserLocation> asBMKUserLocation() async {
     final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKUserLocation', {'refId': refId});
     return BMKUserLocation()..refId = result;
+  }
+  
+  Future<BMKCoordinateSpan> asBMKCoordinateSpan() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKCoordinateSpan', {'refId': refId});
+    return BMKCoordinateSpan()..refId = result;
+  }
+  
+  Future<BMKCoordinateBounds> asBMKCoordinateBounds() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKCoordinateBounds', {'refId': refId});
+    return BMKCoordinateBounds()..refId = result;
+  }
+  
+  Future<BMKCoordinateRegion> asBMKCoordinateRegion() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKCoordinateRegion', {'refId': refId});
+    return BMKCoordinateRegion()..refId = result;
+  }
+  
+  Future<BMKGeoPoint> asBMKGeoPoint() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKGeoPoint', {'refId': refId});
+    return BMKGeoPoint()..refId = result;
+  }
+  
+  Future<BMKMapPoint> asBMKMapPoint() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKMapPoint', {'refId': refId});
+    return BMKMapPoint()..refId = result;
+  }
+  
+  Future<BMKMapSize> asBMKMapSize() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKMapSize', {'refId': refId});
+    return BMKMapSize()..refId = result;
+  }
+  
+  Future<BMKMapRect> asBMKMapRect() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKMapRect', {'refId': refId});
+    return BMKMapRect()..refId = result;
   }
   
   Future<BMKPlanNode> asBMKPlanNode() async {
