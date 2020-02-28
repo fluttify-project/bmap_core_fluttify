1f46d62ee9634204c9d7597017765d27b86d794f
yohom <yohombao@qq.com>
2020-02-28 09:16:58 +0800

    enhance: 加入缺少的文件

--- lib/src/ios/type_op.g.dart
+++ lib/src/ios/type_op.g.dart
@@ -14,7 +14,55 @@ import 'package:foundation_fluttify/foundation_fluttify.dart';
 
 // ignore_for_file: non_constant_identifier_names, camel_case_types, missing_return, unused_import
 extension TypeOpBmapCoreFluttifyIOS on Ref {
+  Future<bool> isBMKUserLocation() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKUserLocation', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKPlanNode() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKPlanNode', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKIndoorPlanNode() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKIndoorPlanNode', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKAddressComponent() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKAddressComponent', {'refId': refId});
+    return result;
+  }
+  
+  Future<bool> isBMKMapManager() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::isKindOfBMKMapManager', {'refId': refId});
+    return result;
+  }
   
 
+  Future<BMKUserLocation> asBMKUserLocation() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKUserLocation', {'refId': refId});
+    return BMKUserLocation()..refId = result;
+  }
+  
+  Future<BMKPlanNode> asBMKPlanNode() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKPlanNode', {'refId': refId});
+    return BMKPlanNode()..refId = result;
+  }
+  
+  Future<BMKIndoorPlanNode> asBMKIndoorPlanNode() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKIndoorPlanNode', {'refId': refId});
+    return BMKIndoorPlanNode()..refId = result;
+  }
+  
+  Future<BMKAddressComponent> asBMKAddressComponent() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKAddressComponent', {'refId': refId});
+    return BMKAddressComponent()..refId = result;
+  }
+  
+  Future<BMKMapManager> asBMKMapManager() async {
+    final result = await MethodChannel('com.fluttify/bmap_core_fluttify').invokeMethod('RefClass::asBMKMapManager', {'refId': refId});
+    return BMKMapManager()..refId = result;
+  }
   
 }
